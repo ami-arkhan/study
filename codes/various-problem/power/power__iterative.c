58efebbf3fec196f50903a5e8b6fbcea1d3bec64
ARK <31149363+ami-arkhan@users.noreply.github.com>
2017-09-27 00:54:20 +0600

    Update and rename power.c to power__iterative.c

---
+++ codes/various-problem/power/power__iterative.c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+double X, result;
+// 10 <= N <= 500000000
+long int N;
+
+// For debugging how many iteration is called to find the solution
+//int iterative_count;
+
+void power()
+{
+	int i;
+
+	// Initialized with 1 for multiplication
+	result = 1;
+	for (i = 1; i <= N; i++)
+	{
+		// For debugging how many test iteration is called to find the solution
+		//iterative_count++;
+
+		result *= X;
+	}
+}
+
+int main()
+{
+	int test, T;
+
+	freopen("input.txt", "r", stdin);
+	scanf("%d\n", &T);
+
+	for (test = 1; test <= T; test++)
+	{
+		// For debugging how many iteration is called to find the solution
+		//iterative_count = 0;
+
+		scanf("%lf %ld\n", &X, &N);
+		power();
+		printf("#%d %0.5lf\n", test, result);
+
+		// For debugging how many iteration is called to find the solution
+		//printf("Iteration called: %d\n", iterative_count);
+	}
+
+	return 0;
+}
