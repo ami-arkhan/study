83e925a8f1a0a77f462e51a87ddcad9900dd4043
ARK <31149363+ami-arkhan@users.noreply.github.com>
2017-09-27 22:45:44 +0600

    Update xor_operations.c

--- codes/basic-problem/bit-operations/xor_operations.c
+++ codes/basic-problem/bit-operations/xor_operations.c
@@ -1,6 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
+void bitPrint(int x)
+{
+	int i;
+	for (i = 7; i >= 0; i--)
+		printf("%d", (x >> i) & 1);
+}
+
 int main()
 {
 	int a = 55;
@@ -8,22 +15,41 @@ int main()
 	int c = 23;
 	int d = 75;
 
+	printf("-------------------------------------------------\n");
+	printf("1. Changing variables without temporary variable:");
+	printf("\n-------------------------------------------------\n\n");
 	printf("Initial values:\n");
 	printf("a = %d, b = %d\n\n", a, b);
 
-	printf("XOR operations: Numbers are exchanged without third variable !!!\n");
+	printf("XOR operations:\n");
 	a = a ^ b;
 	b = a ^ b;
 	a = a ^ b;
-	printf("a = %d, b = %d\n\n", a, b);
+	printf("a = %d, b = %d => Number exchanged.\n", a, b);
 
+	printf("\n--------------------------------------\n");
+	printf("2. Variable Encryption and Decryption:");
+	printf("\n--------------------------------------\n\n");
 	printf("Initial value:\n");
-	printf("c = %d, d = %d\n\n", c, d);
+	printf("c = %d = ", c);
+	bitPrint(c);
+	printf("\nd = %d = ", d);
+	bitPrint(d);
+	printf("\n=====================\n");
+	c = c ^ d;
+	printf("c = c^d = %d = ", c);
+	bitPrint(c);
+	printf(" => Encryption. Number changed.\n", c);
 
+	printf("\nc = %d = ", c);
+	bitPrint(c);
+	printf("\nd = %d = ", d);
+	bitPrint(d);
+	printf("\n=====================\n");
 	c = c ^ d;
-	printf("c^d = %d => Number changed: Encrypted.\n", c);
-	c = c ^ d;
-	printf("c^d = %d => Number returned to it's original: Decrypted.\n\n", c);
+	printf("c = c^d = %d = ", c);
+	bitPrint(c);
+	printf(" => Decryption. Number returned to it's original value.\n\n", c);
 
 	return 0;
 }
