b9f5bd0de9ac9a8fb481d60b4887984138205356
ARK <31149363+ami-arkhan@users.noreply.github.com>
2017-09-29 07:18:43 +0600

    Create insertion_sort.c

---
+++ codes/sorting/insertion-sort/insertion_sort.c
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+#define SIZE 101
+
+int N;
+int data[SIZE];
+
+void readCase()
+{
+	int i;
+	scanf("%d\n", &N);
+	for (i = 0; i < N; i++)
+		scanf("%d ", &data[i]);
+}
+
+void insertionSort()
+{
+	int i, j, key;
+
+	/********************************************************************************
+	*	Insertion Sort:																*
+	*	1. Search for small value and store it.										*
+	*	2. Compare with all previous value and move larger elements one-by-one.		*
+	*	3. Once element is moved, insert stored value into correct location.		*
+	********************************************************************************/
+	for (i = 0; i < N; i++)
+	{
+		// Store small value
+		key = data[i];
+
+		// Compare with all previous value and move elements
+		j = i - 1;
+		while (j >= 0 && data[j] > key)
+		{
+			data[j + 1] = data[j];
+			j--;
+		}
+		// Insert element
+		data[j + 1] = key;
+	}
+}
+
+void print_case()
+{
+	int i;
+	for (i = 0; i < N; i++)
+		printf("%d ", data[i]);
+	printf("\n");
+}
+
+int main()
+{
+	int test, T;
+	freopen("input.txt", "r", stdin);
+	scanf("%d\n", &T);
+
+	for (test = 1; test <= T; test++)
+	{
+		readCase();
+		insertionSort();
+
+		printf("#%d ", test);
+		print_case();
+	}
+}
