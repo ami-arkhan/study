f3efd91cf83c04b13acbe4695431a6cdfa1bbfed
ARK <31149363+ami-arkhan@users.noreply.github.com>
2017-09-29 06:24:51 +0600

    Create quick_sort.c

---
+++ codes/sorting/quick-sort/quick_sort.c
@@ -0,0 +1,95 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+#define SIZE 101
+
+int N;
+int data[SIZE];
+
+void readCase()
+{
+	int i;
+	scanf("%d\n", &N);
+	for (i = 0; i < N; i++)
+		scanf("%d ", &data[i]);
+}
+
+int partition(int low, int high)
+{
+	/********************************************************************************
+	*	1. Make an element as pivot element. Here last element is made as pivot.	*
+	*	2. Move smaller and equal elements to left. When all small elements			*
+	*		are moved, next index position will be the pivot element position,		*
+	*		because all other elements are larger than the pivot element.			*
+	********************************************************************************/
+
+	int pivot = data[high];
+	int index = low;
+	int i, temp;
+
+	for (i = low; i < high; i++)
+	{
+		if (data[i] <= pivot)
+		{
+			temp = data[index];
+			data[index++] = data[i];
+			data[i] = temp;
+		}
+	}
+	temp = data[index];
+	data[index] = data[high];
+	data[high] = temp;
+
+	return index;
+}
+
+void quickSort(int low, int high)
+{
+	/************************************************************************
+	*	Quick Sort:															*
+	*	1. Search for partition index and make partition of left and right.	*
+	*	2. When partition is created, move lower values on one side,		*
+	*		and higher values on the other side.							*
+	*	3. By Divide and Conquer, each partition is sorted.					*
+	*	4. Terminate when partition consists of single element				*
+	************************************************************************/
+
+	if (low < high)
+	{
+		int partitionIndex;
+
+		// Get partition point location.
+		partitionIndex = partition(low, high);
+
+		/****************************************************************
+		*	1. Partition index element is sorted to correct position.	*
+		*	2. So sorting on left and right partition is needed.		*
+		****************************************************************/
+		quickSort(low, partitionIndex - 1);
+		quickSort(partitionIndex + 1, N - 1);
+	}
+}
+
+void print_case()
+{
+	int i;
+	for (i = 0; i < N; i++)
+		printf("%d ", data[i]);
+	printf("\n");
+}
+
+int main()
+{
+	int test, T;
+	freopen("input.txt", "r", stdin);
+	scanf("%d\n", &T);
+
+	for (test = 1; test <= T; test++)
+	{
+		readCase();
+		quickSort(0, N - 1);
+
+		printf("#%d ", test);
+		print_case();
+	}
+}
